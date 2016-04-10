998ace4489434966c0af926a65d695f258115c80
ZenOokami <zaneblalock@gmail.com>
2016-04-10 01:56:03 -0400

    Design update; Functionality Update:
    [activity_password_generator.xml]
    *Action Bar now includes Hamburger menu that activated navigation panel
    *Sliding in from the left now open the navigation panel
    *Rearranged template settings and code structure to fix bugs (bugs that won't be logged as I fixed it before pushing)
    *Listeners and postCreate functions have been added
    *Number input field created (will probably be deprecated once I find out how to pull the value from number selector)
    
    todo:
    *Add about activity
    *Add help activity
    *add button functionality (will tackle this monday)

--- PasswordGenerator/app/src/main/java/org/essenceofzen/passwordgenerator/PasswordGeneratorActivity.java
+++ PasswordGenerator/app/src/main/java/org/essenceofzen/passwordgenerator/PasswordGeneratorActivity.java
@@ -3,6 +3,8 @@ package org.essenceofzen.passwordgenerator;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,6 +13,9 @@ import android.view.MenuItem;
 
 public class PasswordGeneratorActivity extends AppCompatActivity {
 
+    DrawerLayout navigationDrawerLayout; //    For the hamburger menu icon
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +23,33 @@ public class PasswordGeneratorActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabMainAction);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Clearing Field Complete.", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        //For the Navigation drawer ~~~~~~
+        navigationDrawerLayout = (DrawerLayout)findViewById(R.id.navigation_drawer_layout);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, navigationDrawerLayout, toolbar,
+                R.string.drawer_open, R.string.drawer_close);
+
+        //todo: Try and and get the animated "DrawerArrowDrawableToggle" working and replace "actionBarDrawerToggle"
+
+        navigationDrawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+    }
+
+    // Also for hamburger icon
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        actionBarDrawerToggle.syncState();
     }
 
     @Override
